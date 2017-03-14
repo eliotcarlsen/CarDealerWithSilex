d54d3f402489f0cd7ce61070060ea1629ae0b3a6
zach beecher <zachbeecher@gmail.com>
2017-03-14 16:39:19 -0700

    twig conversions

--- app/app.php
+++ app/app.php
@@ -4,34 +4,14 @@
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-      return '<!DOCTYPE html>
-      <html>
-        <head>
-          <link href="css/bootstrap.css" type="text/css" rel="stylesheet">
-          <link href="css/styles.css" type="text/css" rel="stylesheet">
-          <title>Car Dealer</title>
-        </head>
-        <body>
-          <div class="container">
-            <h1>Search our inventory</h1>
-            <form id="userInput" action="/car">
-              <div class="form-group">
-                <label for="priceInput">What is your max price?</label>
-                <input id="priceInput" name="priceInput" class="form-control" type="number">
-              </div>
-              <div class="form-group">
-                <label for="milageInput">What is your max milage?</label>
-                <input id="milageInput" name="milageInput" class="form-control" type="number">
-              </div>
-              <div class="form-group">
-                <button type="submit" class="btn btn-default">Show me cars!</button>
-              </div>
-            </form>
-        </body>
-      </html>';
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
+  ));
+
+    $app->get("/", function() use ($app) {
+      return $app['twig']->render('cars.html.twig');
     });
-    $app->get("/car", function() {
+
+    $app->get("/car", function() use ($app) {
       $first_car = new Car("2014 Porshe 911", "114220", "7622", "img/car1.jpeg");
       $second_car = new Car("2011 Ford F350", "12333", "44223", "img/car2.jpeg");
       $third_car = new Car("2013 Lexus RX 350", "44700", "20000", "img/car3.jpeg");
@@ -48,20 +28,7 @@
             array_push($cars_matching_search, $car);
           }
         }
-        if (empty($cars_matching_search)) {
-            echo "<p>You're too picky about cars...</p>";
-        } else {
-              foreach ($cars_matching_search as $car) {
-                  $miles = $car->getMiles();
-                  $price = $car->getPrice();
-                  $make_model = $car->getMakeModel();
-                  $car_pic = $car->getCarPic();
-
-                  echo "<li>" . $make_model . "</li><ul><li> Price:" . $price . "</li><li> Miles: " . $miles . "</li></ul><div class='row'>
-                    <div class='col-md-6'>
-                      <img src='" . $car_pic . "'>";
-            }
-          };
+        return $app['twig']->render('showcars.html.twig', array('old_cars' => $cars_matching_search));
     });
     return $app;
 ?>
